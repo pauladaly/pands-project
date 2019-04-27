274469d068274478efae119d7804b6251f8832ff
Daly <paula.daly@dxc.com>
2019-04-27 16:39:49 +0100

    scatter

--- pands-project.py
+++ pands-project.py
@@ -18,3 +18,21 @@ print (iris.columns)
 
 # How many Flowers for each Species?
 iris["species"].value_counts()
+
+# 2-D Scatter Plot
+iris.plot(kind='scatter', x='sepal_length', y='sepal_width');plt.show()
+
+# 2-D Scatter Plot with colour-coding for each flower type/class
+# Using sns from seaborn
+
+sns.set_style("whitegrid");
+sns.FacetGrid(iris, hue="species", size=4) \
+    .map(plt.scatter, "sepal_length", "sepal_width") \
+    .add_legend();
+plt.show();    
+
+# Pair-Plot
+iris.plot(kind='scatter', x='sepal_length', y='sepal_width');plt.show()
+sns.set_style("whitegrid");
+sns.pairplot(iris, hue="species", size=3);
+plt.show()
